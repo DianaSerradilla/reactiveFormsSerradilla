ad03a9754cc650c34bf1f639d955c7398ddb7565
DianaSerradilla <serradilladiana3@gmail.com>
2022-07-04 22:03:58 -0300

    Arreglar errores

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,34 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.scss']
 })
-export class  FormularioComponent implements OnInit {
-  
-  estadoFormulario : string = 'incompleto';
+export class FormularioComponent implements OnInit {
 
-  public formulario: FormGroup;
+  formulario!: FormGroup;
+
   constructor(
     private fb: FormBuilder
-  ) {
-    this.formulario = this.fb.group({
-      nombre: [],
-      apellido: [],
-      usuario: [],
-      email: [],
-      dni: [],
-      condiciones: []
-   });
-  }
-
+  ) { }
   ngOnInit(): void {
+    // this.formulario = this.fb.group({
+    //   nombre: ['valor inicial', [Validators.required]],
+    //   apellido: [null, [Validators.required]],
+    //   usuario: [null, [Validators.required]],
+    //   email: ['', [Validators.required, Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]],
+    //   dni: [null, [Validators.required]],
+    //   condiciones: [null, [Validators.required]]
+    // });
+
+
+    this.formulario = new FormGroup({
+      'nombre': new FormControl(null, [Validators.required, Validators.pattern('[-_a-zA-Z]*')]),
+      'apellido': new FormControl(null, [Validators.required, Validators.pattern('[-_a-zA-Z]*')]),
+      'usuario': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]),
+      'dni': new FormControl(null, [Validators.required, Validators.maxLength(8)]),
+      'condiciones': new FormControl(null, Validators.required),
+    })
   }
 
-
-  submit(valorFormulario: any){
-    console.log(this.formulario);
+  submit(valorFormulario: any) {
+    console.log(this.formulario.value);
+    console.log(this.formulario.get('nombre')?.errors?.['required']);
   }
 }
